64ee4455abf3adc5a764e002a966cc96b99deefb
brandanblevins <brandan.blevins@gmail.com>
2022-12-07 12:31:53 -0700

    Add ideal image config settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,3 +130,19 @@ const config = {
 };
 
 module.exports = config;
+
+//Config settings for ideal-image plugin.
+module.exports = {
+  plugins: [
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        disableInDev: false,
+      },
+    ],
+  ],
+};
